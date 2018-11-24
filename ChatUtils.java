c5df8421cbbb062ff5a8d3ab9c2507d9cfaa5e65
MrCreeperSteve <Ivan.Smolyak467@gmail.com>
2018-11-24 18:15:12 +0300

    Delete ChatUtils.java

--- ChatUtils.java
+++
@@ -1,9 +0,0 @@
-package me.NukerFall.SuperHub.Utils;
-
-import net.md_5.bungee.api.ChatColor;
-
-public class ChatUtils {
-	public static String chat(String s) {
-		return ChatColor.translateAlternateColorCodes('&', s);
-		}
-	}
